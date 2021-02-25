73301e33a914dc42e38ffb7988d87542c38f3e47
Mark Battistella <markbattistella@users.noreply.github.com>
2021-02-25 19:58:07 +1100

    cleanup fixes

---
+++ 03 - RandomCard-Storyboard/RandomCard-Storyboard/CardSelectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  RandomCard-Storyboard
+//
+//  Created by Mark Battistella on 27/9/20.
+//
+
+import UIKit
+
+class CardSelectionViewController: UIViewController {
+
+	// variables
+	@IBOutlet var cardImage: UIImageView!
+	@IBOutlet var startStopButton: UIButton!
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+	
+	
+	// 
+	@IBAction func startStopButtonTapped(_ sender: UIButton) {
+	}
+	
+
+}
+
